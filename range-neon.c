e32228afea811bcf46757e426ff29696d9260069
Yibo Cai <cyb70289@gmail.com>
2018-11-13 00:33:36 +0800

    optimize range-neon.c

--- range-neon.c
+++ range-neon.c
@@ -50,6 +50,12 @@ static void print128(const char *s, const uint8x16_t v128)
 }
 #endif
 
+struct previous_input {
+    uint8x16_t input;
+    uint8x16_t follow_bytes;
+    int        follow_max;
+};
+
 static const uint8_t _followup_tbl[] = {
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 00 ~ BF */
     1, 1,                               /* C0 ~ DF */
@@ -59,8 +65,8 @@ static const uint8_t _followup_tbl[] = {
 
 static const uint8_t _range_tbl[] = {
     /* 0,    1,    2,    3,    4,    5,    6,    7 */
-    0x00, 0x80, 0xA0, 0x80, 0x90, 0x80, 0xC2, 0x00, /* min */
-    0x7F, 0xBF, 0xBF, 0x9F, 0xBF, 0x8F, 0xF4, 0xFF, /* max */
+    0x00, 0x80, 0x80, 0x80, 0xC2, 0xFF, 0xFF, 0xFF, /* min */
+    0x7F, 0xBF, 0xBF, 0xBF, 0xF4, 0x00, 0x00, 0x00, /* max */
 };
 
 /* Get number of followup bytes to take care per high nibble */
@@ -72,123 +78,122 @@ static inline uint8x16_t get_followup_bytes(const uint8x16_t input)
     return vqtbl1q_u8(followup_table, high_nibbles);
 }
 
-static inline uint8x16_t validate(const unsigned char *data, uint8x16_t *error,
-                                  uint8x16_t range0)
+static inline uint8x16_t validate(const unsigned char *data, uint8x16_t error,
+        struct previous_input *prev)
 {
     const uint8x16_t input = vld1q_u8(data);
 
-    const uint8x16_t zero = vdupq_n_u8(0);
-    const uint8x16_t one = vdupq_n_u8(1);
+    const uint8x16_t follow_bytes = get_followup_bytes(input);
+    const uint8x16_t follow_mask = vcgtq_u8(follow_bytes, vdupq_n_u8(0));
+    uint8x16_t prev_follow_bytes, range, tmp;
 
-    uint8x16_t fi0 = get_followup_bytes(input);
-    uint8x16_t fi1;
-    uint8x16_t range1 = zero;
-    uint8x16_t mask0, mask1;
-    const uint8x16_t input_mask = vcgtq_u8(fi0, zero); /* where input >= C0 */
-    const uint8x16_t range0_mask = vcgtq_u8(range0, zero); /* where range > 0 */
+    const int follow_max = vmaxvq_u8(follow_bytes);
 
-    /* range0 |= (fi0 > 0) & 6 */
-    range0 = vorrq_u8(range0, vandq_u8(input_mask, vdupq_n_u8(6)));
-
-    int followup_bytes = vmaxvq_u8(fi0);
-
-    if (followup_bytes--) {
-        /* Second byte */
-        /* (mask1, mask0) = (0, fi0) << 8 */
-        mask1 = vextq_u8(fi0, zero, 15);
-        mask0 = vextq_u8(zero, fi0, 15);
-
-        /* Third byte */
-        if (followup_bytes--) {
-            /* Third byte */
-            /* fi = (fi >= 1 ? fi-1 : 0) */
-            fi0 = vqsubq_u8(fi0, one);
-
-            /* (fi1, fi0) <<= 16 */
-            fi1 = vextq_u8(fi0, zero, 14);
-            fi0 = vextq_u8(zero, fi0, 14);
-
-            mask1 = vorrq_u8(mask1, fi1);
-            mask0 = vorrq_u8(mask0, fi0);
-
-            /* Fourth byte */
-            if (followup_bytes) {
-                /* fi = (fi >= 1 ? fi-1 : 0) */
-                fi1 = vqsubq_u8(fi1, one);
-                fi0 = vqsubq_u8(fi0, one);
-
-                /* (fi1, fi0) <<= 8 */
-                fi1 = vextq_u8(fi0, fi1, 15);
-                fi0 = vextq_u8(zero, fi0, 15);
-
-                mask1 = vorrq_u8(mask1, fi1);
-                mask0 = vorrq_u8(mask0, fi0);
-            }
-
-            /* mask[mask==3,2,1] = 1 */
-            mask1 = vandq_u8(vcgtq_u8(mask1, zero), one);
-            mask0 = vandq_u8(vcgtq_u8(mask0, zero), one);
-
-            /*
-             * Second byte: deal with special cases (not 80..BF)
-             * +------------+---------------------+-------------------+
-             * | First Byte | Special Second Byte | range table index |
-             * +------------+---------------------+-------------------+
-             * | E0         | A0..BF              | 2                 |
-             * | ED         | 80..9F              | 3                 |
-             * | F0         | 90..BF              | 4                 |
-             * | F4         | 80..8F              | 5                 |
-             * +------------+---------------------+-------------------+
-             */
-            /* fi0 = (input == 0xE0) & 1 */
-            fi1 = vceqq_u8(input, vdupq_n_u8(0xE0));
-            fi0 = vandq_u8(fi1, one);
-            /* fi0 += (input == 0xED) & 2 */
-            fi1 = vceqq_u8(input, vdupq_n_u8(0xED));
-            fi0 = vaddq_u8(fi0, vandq_u8(fi1, vdupq_n_u8(2)));
-            /* fi0 += (input == 0xF0) & 3 */
-            fi1 = vceqq_u8(input, vdupq_n_u8(0xF0));
-            fi0 = vaddq_u8(fi0, vandq_u8(fi1, vdupq_n_u8(3)));
-            /* fi0 += (input == 0xF4) & 4 */
-            fi1 = vceqq_u8(input, vdupq_n_u8(0xF4));
-            fi0 = vaddq_u8(fi0, vandq_u8(fi1, vdupq_n_u8(4)));
-            /* (fi1, fi0) = (0, fi0) << 8 */
-            fi1 = vextq_u8(fi0, zero, 15);
-            fi0 = vextq_u8(zero, fi0, 15);
-
-            mask1 = vaddq_u8(mask1, fi1);
-            mask0 = vaddq_u8(mask0, fi0);
-        }
-
-        /* range += mask */
-        range1 = mask1;
-        range0 = vaddq_u8(range0, mask0);
-
-        /* Check overlap */
-        mask0 = vorrq_u8(range0_mask, vcgtq_u8(mask0, zero));
-        *error = vorrq_u8(*error, vandq_u8(mask0, input_mask));
+    if (follow_max == 0 && prev->follow_max == 0) {
+        /* All ascii */
+        prev->input = input;
+        return vorrq_u8(error, vcgtq_u8(input, vdupq_n_u8(0x7F)));
     }
 
-    fi0 = vld1q_u8(_range_tbl);
-    mask0 = vqtbl1q_u8(fi0, range0);
-    mask1 = vqtbl1q_u8(fi0, vaddq_u8(range0, vdupq_n_u8(8)));
+    /* 2nd byte */
+    /* range = (follow_bytes, prev.follow_bytes) << 1 byte */
+    range = vextq_u8(prev->follow_bytes, follow_bytes, 15);
+
+    /* 3rd bytes */
+    /* saturate sub 1 */
+    tmp = vqsubq_u8(follow_bytes, vdupq_n_u8(1));
+    prev_follow_bytes = vqsubq_u8(prev->follow_bytes, vdupq_n_u8(1));
+    /* range |= (tmp, prev_follow_bytes) << 2 bytes */
+    tmp = vextq_u8(prev_follow_bytes, tmp, 14);
+    range = vorrq_u8(range, tmp);
+
+    /* 4th bytes */
+    /* saturate sub 2 */
+    tmp = vqsubq_u8(follow_bytes, vdupq_n_u8(2));
+    prev_follow_bytes = vqsubq_u8(prev->follow_bytes, vdupq_n_u8(2));
+    /* range |= (tmp, prev_follow_bytes) << 3 bytes */
+    tmp = vextq_u8(prev_follow_bytes, tmp, 13);
+    range = vorrq_u8(range, tmp);
+
+#if 1
+    /* If token overlaps, range will be 5,6,7 of bad range */
+    range = vaddq_u8(range, vandq_u8(follow_mask, vdupq_n_u8(4)));
+#else
+    /* Check overlap */
+    error = vorrq_u8(error, vandq_u8(range, follow_mask));
+    /* range = 4 if follow_mask else 0 */
+    range = vorrq_u8(range, vandq_u8(follow_mask, vdupq_n_u8(4)));
+#endif
+
+    /* Check value range */
+    tmp = vld1q_u8(_range_tbl);
+    uint8x16_t minv = vqtbl1q_u8(tmp, range);
+    uint8x16_t maxv = vqtbl1q_u8(tmp, vaddq_u8(range, vdupq_n_u8(8)));
 
     /* errors |= ((input < min) | (input > max)) */
-    *error = vorrq_u8(*error, vcltq_u8(input, mask0));
-    *error = vorrq_u8(*error, vcgtq_u8(input, mask1));
+    error = vorrq_u8(error, vcltq_u8(input, minv));
+    error = vorrq_u8(error, vcgtq_u8(input, maxv));
 
-    return range1;
+#if 0
+    if (vmaxvq_u8(error)) {
+        print128("pinput", prev->input);
+        print128("pbytes", prev->follow_bytes);
+        print128("input", input);
+        print128("range", range);
+        exit(1);
+    }
+#endif
+
+    /*
+     * Check special cases (not 80..BF)
+     * +------------+---------------------+-------------------+
+     * | First Byte | Special Second Byte | range table index |
+     * +------------+---------------------+-------------------+
+     * | E0         | A0..BF              | 2                 |
+     * | ED         | 80..9F              | 3                 |
+     * | F0         | 90..BF              | 4                 |
+     * | F4         | 80..8F              | 5                 |
+     * +------------+---------------------+-------------------+
+     */
+    /* tmp = (input, prev.input) << 1 byte */
+    tmp = vextq_u8(prev->input, input, 15);
+    /* tmp == 0xE0 && input < 0xA0 */
+    error = vorrq_u8(error,
+                     vandq_u8(vceqq_u8(tmp, vdupq_n_u8(0xE0)),
+                              vcltq_u8(input, vdupq_n_u8(0xA0))));
+    /* tmp == 0xED && input > 0x9F */
+    error = vorrq_u8(error,
+                     vandq_u8(vceqq_u8(tmp, vdupq_n_u8(0xED)),
+                              vcgtq_u8(input, vdupq_n_u8(0x9F))));
+    /* tmp == 0xF0 && input < 0x90 */
+    error = vorrq_u8(error,
+                     vandq_u8(vceqq_u8(tmp, vdupq_n_u8(0xF0)),
+                              vcltq_u8(input, vdupq_n_u8(0x90))));
+    /* tmp == 0xF4 && input > 0x8F */
+    error = vorrq_u8(error,
+                     vandq_u8(vceqq_u8(tmp, vdupq_n_u8(0xF4)),
+                              vcgtq_u8(input, vdupq_n_u8(0x8F))));
+
+    prev->input = input;
+    prev->follow_bytes = follow_bytes;
+    prev->follow_max = follow_max;
+
+    return error;
 }
 
 int utf8_range(const unsigned char *data, int len)
 {
     if (len >= 16) {
-        uint32_t range3;
-        uint8x16_t range = vdupq_n_u8(0);
+        struct previous_input previous_input;
+
+        previous_input.input = vdupq_n_u8(0);
+        previous_input.follow_bytes = vdupq_n_u8(0);
+        previous_input.follow_max = 0;
+
         uint8x16_t error = vdupq_n_u8(0);
 
         while (len >= 16) {
-            range = validate(data, &error, range);
+            error = validate(data, error, &previous_input);
 
             data += 16;
             len -= 16;
@@ -198,21 +203,20 @@ int utf8_range(const unsigned char *data, int len)
         if (vmaxvq_u8(error))
             return 0;
 
-        /* At most three followup bytes need to take care */
-        vst1q_lane_u32(&range3, vreinterpretq_u32_u8(range), 0);
+        /* Find previous token (not 80~BF) */
+        uint32_t token4;
+        vst1q_lane_u32(&token4, vreinterpretq_u32_u8(previous_input.input), 3);
 
-        while (range3) {
-            uint8_t idx = range3 & 7;
-
-            if (len == 0)
-                return 0;
-            if (*data < _range_tbl[idx] || *data > _range_tbl[idx+8])
-                return 0;
-
-            --len;
-            ++data;
-            range3 >>= 8;
-        }
+        const int8_t *token = (const int8_t *)&token4;
+        int lookahead = 0;
+        if (token[3] > (int8_t)0xBF)
+            lookahead = 1;
+        else if (token[2] > (int8_t)0xBF)
+            lookahead = 2;
+        else if (token[1] > (int8_t)0xBF)
+            lookahead = 3;
+        data -= lookahead;
+        len += lookahead;
     }
 
     /* Check remaining bytes with naive method */
@@ -230,8 +234,7 @@ int main(void)
      *  "\x00\x00\x00\x00\x00\xc2\xc2\x80\x00\x00\xe1\x80\x80\x00\x00\x00";
      */
     const unsigned char src[] =
-        "\x00\x00\x00\x00\x00\xc2\xc2\x80\x00\x00\xe1\x80\x80\x00\x00\x00";
-
+        "\x00\x66\x7F\x00\x7F\x7F\x00\xC2\x80\xDF\xBF\xE0\xA0\x80\xE0\xA0\xBF";
     int ret = utf8_range(src, sizeof(src)-1);
     printf("%s\n", ret ? "ok": "bad");
 
