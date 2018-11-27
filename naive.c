4de2918b7480531e5438e65eab780e4b27ad5010
Yibo Cai <cyb70289@gmail.com>
2018-11-27 15:59:29 +0800

    optimize naive approach

--- naive.c
+++ naive.c
@@ -28,68 +28,62 @@
  * +--------------------+------------+-------------+------------+-------------+
  */
 
-/*
- * input: *len >= 1
- * return utf-8 char length, 0 or error
- */
-static const unsigned char *validate(const unsigned char *data, int *len)
-{
-    unsigned char b1, b2;
-
-    b1 = *data++;
-    --(*len);
-
-    /* 00-7F */
-    if (b1 <= 0x7F)
-        return data;
-
-    /* Invalid code: 80-C1, F5-FF */
-    if (b1 < 0xC2 || b1 > 0xF4)
-        return NULL;
-
-    /* 2~4 bytes, 2nd byte = 80-BF */
-    if (*len == 0)
-        return NULL;
-    b2 = *data++;
-    --(*len);
-    if (b2 < 0x80 || b2 > 0xBF)
-        return NULL;
-
-    /* C2-DF, 2 bytes */
-    if (b1 < 0xE0)
-        return data;
-
-    /* 3~4 bytes, 3rd byte = 80-BF */
-    if (*len == 0)
-        return NULL;
-    if (*data < 0x80 || *data > 0xBF)
-        return NULL;
-    ++data;
-    --(*len);
-
-    /* Special cases: E0, A0-BF; ED, 80-9F; F0, 90-BF; F4, 80-8F */
-    if ((b1 == 0xE0 && b2 < 0xA0) || (b1 == 0xED && b2 > 0x9F) ||
-        (b1 == 0xF0 && b2 < 0x90) || (b1 == 0xF4 && b2 > 0x8F))
-        return NULL;
-
-    /* E0-EF, 3 bytes*/
-    if (b1 < 0xF0)
-        return data;
-
-    /* F0-F4, 4 bytes, 4th byte = 80-BF */
-    if (*len == 0)
-        return NULL;
-    if (*data < 0x80 || *data > 0xBF)
-        return NULL;
-    --(*len);
-    return data+1;
-}
-
 /* return 0-invalid, 1-valid */
 int utf8_naive(const unsigned char *data, int len)
 {
-    while (len && data)
-        data = validate(data, &len);
+    while (len) {
+        int bytes;
+        const unsigned char byte1 = data[0];
 
-    return !!data;
+        /* 00..7F */
+        if (byte1 <= 0x7F) {
+            bytes = 1;
+        /* C2..DF, 80..BF */
+        } else if (len >= 2 && byte1 >= 0xC2 && byte1 <= 0xDF &&
+                (signed char)data[1] <= (signed char)0xBF) {
+            bytes = 2;
+        } else if (len >= 3) {
+            const unsigned char byte2 = data[1];
+
+            /* Is byte2, byte3 between 0x80 ~ 0xBF */
+            const int byte2_ok = (signed char)byte2 <= (signed char)0xBF;
+            const int byte3_ok = (signed char)data[2] <= (signed char)0xBF;
+
+            if (byte2_ok && byte3_ok &&
+                     /* E0, A0..BF, 80..BF */
+                    ((byte1 == 0xE0 && byte2 >= 0xA0) ||
+                     /* E1..EC, 80..BF, 80..BF */
+                     (byte1 >= 0xE1 && byte1 <= 0xEC) ||
+                     /* ED, 80..9F, 80..BF */
+                     (byte1 == 0xED && byte2 <= 0x9F) ||
+                     /* EE..EF, 80..BF, 80..BF */
+                     (byte1 >= 0xEE && byte1 <= 0xEF))) {
+                bytes = 3;
+            } else if (len >= 4) {
+                /* Is byte4 between 0x80 ~ 0xBF */
+                const int byte4_ok = (signed char)data[3] <= (signed char)0xBF;
+
+                if (byte2_ok && byte3_ok && byte4_ok &&
+                         /* F0, 90..BF, 80..BF, 80..BF */
+                        ((byte1 == 0xF0 && byte2 >= 0x90) ||
+                         /* F1..F3, 80..BF, 80..BF, 80..BF */
+                         (byte1 >= 0xF1 && byte1 <= 0xF3) ||
+                         /* F4, 80..8F, 80..BF, 80..BF */
+                         (byte1 == 0xF4 && byte2 <= 0x8F))) {
+                    bytes = 4;
+                } else {
+                    return 0;
+                }
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+
+        len -= bytes;
+        data += bytes;
+    }
+
+    return 1;
 }
