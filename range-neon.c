99dbec6fcb696010709940717cd2676d0cfca1fb
Yibo Cai <cyb70289@gmail.com>
2018-11-30 10:17:19 +0000

    refine range-neon

--- range-neon.c
+++ range-neon.c
@@ -20,42 +20,65 @@ static void print128(const char *s, const uint8x16_t v128)
 }
 #endif
 
-struct previous_input {
-    uint8x16_t input;
-    uint8x16_t follow_bytes;
+/*
+ * Map high nibble of "First Byte" to legal character length minus 1
+ * 0x00 ~ 0xBF --> 0
+ * 0xC0 ~ 0xDF --> 1
+ * 0xE0 ~ 0xEF --> 2
+ * 0xF0 ~ 0xFF --> 3
+ */
+static const uint8_t _first_len_tbl[] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3,
 };
 
-static const uint8_t _follow_tbl[] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 00 ~ BF */
-    1, 1,                               /* C0 ~ DF */
-    2,                                  /* E0 ~ EF */
-    3,                                  /* F0 ~ FF */
+/* Map "First Byte" to 8-th item of range table (0xC2 ~ 0xF4) */
+static const uint8_t _first_range_tbl[] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8,
 };
 
-static const uint8_t _follow_mask_tbl[] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 00 ~ BF */
-    8, 8,                               /* C0 ~ DF */
-    8,                                  /* E0 ~ EF */
-    8,                                  /* F0 ~ FF */
-};
-
+/*
+ * Range table, map range index to min and max values
+ * Index 0    : 00 ~ 7F (First Byte, ascii)
+ * Index 1,2,3: 80 ~ BF (Second, Third, Fourth Byte)
+ * Index 4    : A0 ~ BF (Second Byte after E0)
+ * Index 5    : 80 ~ 9F (Second Byte after ED)
+ * Index 6    : 90 ~ BF (Second Byte after F0)
+ * Index 7    : 80 ~ 8F (Second Byte after F4)
+ * Index 8    : C2 ~ F4 (First Byte, non ascii)
+ * Index 9~15 : illegal: u >= 255 && i <= 0
+ */
 static const uint8_t _range_min_tbl[] = {
-    /* 0,    1,    2,    3,    4,    5,    6,    7,    8 */
-    0x00, 0x80, 0x80, 0x80, 0xA0, 0x80, 0x90, 0x80, 0xC2,
-    /* Must be invalid */
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+    0x00, 0x80, 0x80, 0x80, 0xA0, 0x80, 0x90, 0x80,
+    0xC2, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
 };
-
 static const uint8_t _range_max_tbl[] = {
-    /* 0,    1,    2,    3,    4,    5,    6,    7,    8 */
-    0x7F, 0xBF, 0xBF, 0xBF, 0xBF, 0x9F, 0xBF, 0x8F, 0xF4,
-    /* Must be invalid */
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x7F, 0xBF, 0xBF, 0xBF, 0xBF, 0x9F, 0xBF, 0x8F,
+    0xF4, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 };
 
-/* uint8x16x2_t requires an interleaved table */
-static const uint8_t _minus_e0_tbl[] = {
-    /* index: 0~15, 16~31            */
+/*
+ * This table is for fast handling four special First Bytes(E0,ED,F0,F4), after
+ * which the Second Byte are not 80~BF. It contains "range index adjustment".
+ * - The idea is to minus byte with E0, use the result(0~31) as the index to
+ *   lookup the "range index adjustment". Then add the adjustment to original
+ *   range index to get the correct range.
+ * - Range index adjustment
+ *   +------------+---------------+------------------+----------------+
+ *   | First Byte | original range| range adjustment | adjusted range |
+ *   +------------+---------------+------------------+----------------+
+ *   | E0         | 2             | 2                | 4              |
+ *   +------------+---------------+------------------+----------------+
+ *   | ED         | 2             | 3                | 5              |
+ *   +------------+---------------+------------------+----------------+
+ *   | F0         | 3             | 3                | 6              |
+ *   +------------+---------------+------------------+----------------+
+ *   | F4         | 4             | 4                | 8              |
+ *   +------------+---------------+------------------+----------------+
+ * - Below is a uint8x16x2 table, data is interleaved in NEON register. So I'm
+ *   putting it vertically. 1st column is for E0~EF, 2nd column for F0~FF.
+ */
+static const uint8_t _range_adjust_tbl[] = {
+    /* index -> 0~15  16~31 <- index */
     /*  E0 -> */ 2,     3, /* <- F0  */
                  0,     0,
                  0,     0,
@@ -74,92 +97,96 @@ static const uint8_t _minus_e0_tbl[] = {
                  0,     0,
 };
 
-static inline uint8x16_t validate(const unsigned char *data, uint8x16_t error,
-        struct previous_input *prev, const uint8x16_t tables[],
-        const uint8x16x2_t minus_e0)
-{
-    const uint8x16_t input = vld1q_u8(data);
-
-    const uint8x16_t high_nibbles = vshrq_n_u8(input, 4);
-    const uint8x16_t follow_bytes = vqtbl1q_u8(tables[0], high_nibbles);
-
-    /* range is 8 if input=0xC0~0xFF, overlap will lead to 9, 10, 11 */
-    uint8x16_t range = vqtbl1q_u8(tables[1], high_nibbles);
-
-    /* 2nd byte */
-    /* range |= (follow_bytes, prev.follow_bytes) << 1 byte */
-    range = vorrq_u8(range, vextq_u8(prev->follow_bytes, follow_bytes, 15));
-
-    /* 3rd byte */
-    uint8x16_t tmp, prev_follow_bytes;
-    /* saturate sub 1 */
-    tmp = vqsubq_u8(follow_bytes, vdupq_n_u8(1));
-    prev_follow_bytes = vqsubq_u8(prev->follow_bytes, vdupq_n_u8(1));
-    /* range |= (tmp, prev_follow_bytes) << 2 bytes */
-    tmp = vextq_u8(prev_follow_bytes, tmp, 14);
-    range = vorrq_u8(range, tmp);
-
-    /* 4th byte */
-    /* saturate sub 2 */
-    tmp = vqsubq_u8(follow_bytes, vdupq_n_u8(2));
-    prev_follow_bytes = vqsubq_u8(prev->follow_bytes, vdupq_n_u8(2));
-    /* range |= (tmp, prev_follow_bytes) << 3 bytes */
-    tmp = vextq_u8(prev_follow_bytes, tmp, 13);
-    range = vorrq_u8(range, tmp);
-
-    /*
-     * Check special cases (not 80..BF)
-     * +------------+---------------------+-------------------+
-     * | First Byte | Special Second Byte | range table index |
-     * +------------+---------------------+-------------------+
-     * | E0         | A0..BF              | 4=2+2             |
-     * | ED         | 80..9F              | 5=2+3             |
-     * | F0         | 90..BF              | 6=3+3             |
-     * | F4         | 80..8F              | 7=3+4             |
-     * +------------+---------------------+-------------------+
-     */
-    /* shift1 = (input, prev.input) << 1 byte */
-    uint8x16_t shift1 = vextq_u8(prev->input, input, 15);
-    /* E0: +2    ED: +3    F0: +3    F4: +4 */
-    uint8x16_t pos = vsubq_u8(shift1, vdupq_n_u8(0xE0));
-    range = vaddq_u8(range, vqtbl2q_u8(minus_e0, pos));
-
-    /* Check value range */
-    uint8x16_t minv = vqtbl1q_u8(tables[2], range);
-    uint8x16_t maxv = vqtbl1q_u8(tables[3], range);
-
-    /* errors |= ((input < min) | (input > max)) */
-    error = vorrq_u8(error, vcltq_u8(input, minv));
-    error = vorrq_u8(error, vcgtq_u8(input, maxv));
-
-    prev->input = input;
-    prev->follow_bytes = follow_bytes;
-
-    return error;
-}
-
+// 2x ~ 4x faster than naive method
 int utf8_range(const unsigned char *data, int len)
 {
     if (len >= 16) {
-        struct previous_input previous_input;
+        uint8x16_t prev_input = vdupq_n_u8(0);
+        uint8x16_t prev_first_len = vdupq_n_u8(0);
 
-        previous_input.input = vdupq_n_u8(0);
-        previous_input.follow_bytes = vdupq_n_u8(0);
+        /* Cached tables */
+        const uint8x16_t first_len_tbl = vld1q_u8(_first_len_tbl);
+        const uint8x16_t first_range_tbl = vld1q_u8(_first_range_tbl);
+        const uint8x16_t range_min_tbl = vld1q_u8(_range_min_tbl);
+        const uint8x16_t range_max_tbl = vld1q_u8(_range_max_tbl);
+        const uint8x16x2_t range_adjust_tbl = vld2q_u8(_range_adjust_tbl);
 
-        /* Cached constant tables */
-        uint8x16_t tables[4];
-
-        tables[0] = vld1q_u8(_follow_tbl);
-        tables[1] = vld1q_u8(_follow_mask_tbl);
-        tables[2] = vld1q_u8(_range_min_tbl);
-        tables[3] = vld1q_u8(_range_max_tbl);
-
-        uint8x16x2_t minus_e0 = vld2q_u8(_minus_e0_tbl);
+        /* Cached values */
+        const uint8x16_t const_1 = vdupq_n_u8(1);
+        const uint8x16_t const_2 = vdupq_n_u8(2);
+        const uint8x16_t const_e0 = vdupq_n_u8(0xE0);
 
         uint8x16_t error = vdupq_n_u8(0);
 
         while (len >= 16) {
-            error = validate(data, error, &previous_input, tables, minus_e0);
+            const uint8x16_t input = vld1q_u8(data);
+            const uint8x16_t high_nibbles = vshrq_n_u8(input, 4);
+
+            /* first_len = legal character length minus 1 */
+            /* 0 for 00~7F, 1 for C0~DF, 2 for E0~EF, 3 for F0~FF */
+            const uint8x16_t first_len =
+                vqtbl1q_u8(first_len_tbl, high_nibbles);
+
+            /* First Byte: set range index to 8 for bytes within 0xC0 ~ 0xFF */
+            /* range = first_range_tbl[high_nibbles] */
+            uint8x16_t range = vqtbl1q_u8(first_range_tbl, high_nibbles);
+
+            /* Second Byte: set range index to first_len */
+            /* 0 for 00~7F, 1 for C0~DF, 2 for E0~EF, 3 for F0~FF */
+            /* range |= (first_len, prev_first_len) << 1 byte */
+            range =
+                vorrq_u8(range, vextq_u8(prev_first_len, first_len, 15));
+
+            /* Third Byte: set range index to saturate_sub(first_len, 1) */
+            /* 0 for 00~7F, 0 for C0~DF, 1 for E0~EF, 2 for F0~FF */
+            uint8x16_t tmp1, tmp2;
+            /* tmp1 = saturate_sub(first_len, 1) */
+            tmp1 = vqsubq_u8(first_len, const_1);
+            /* tmp2 = saturate_sub(prev_first_len, 1) */
+            tmp2 = vqsubq_u8(prev_first_len, const_1);
+            /* range |= (tmp1, tmp2) << 2 bytes */
+            range = vorrq_u8(range, vextq_u8(tmp2, tmp1, 14));
+
+            /* Fourth Byte: set range index to saturate_sub(first_len, 2) */
+            /* 0 for 00~7F, 0 for C0~DF, 0 for E0~EF, 1 for F0~FF */
+            /* tmp1 = saturate_sub(first_len, 2) */
+            tmp1 = vqsubq_u8(first_len, const_2);
+            /* tmp2 = saturate_sub(prev_first_len, 2) */
+            tmp2 = vqsubq_u8(prev_first_len, const_2);
+            /* range |= (tmp1, tmp2) << 3 bytes */
+            range = vorrq_u8(range, vextq_u8(tmp2, tmp1, 13));
+
+            /*
+             * Now we have below range indices caluclated
+             * Correct cases:
+             * - 8 for C0~FF
+             * - 3 for 1st byte after F0~FF
+             * - 2 for 1st byte after E0~EF or 2nd byte after F0~FF
+             * - 1 for 1st byte after C0~DF or 2nd byte after E0~EF or
+             *         3rd byte after F0~FF
+             * - 0 for others
+             * Error cases:
+             *   9,10,11 if non ascii First Byte overlaps
+             *   E.g., F1 80 C2 90 --> 8 3 10 2, where 10 indicates error
+             */
+
+            /* Adjust Second Byte range for special First Bytes(E0,ED,F0,F4) */
+            /* See _range_adjust_tbl[] definition for details */
+            /* Overlaps lead to index 9~15, which are illegal in range table */
+            uint8x16_t shift1 = vextq_u8(prev_input, input, 15);
+            uint8x16_t pos = vsubq_u8(shift1, const_e0);
+            range = vaddq_u8(range, vqtbl2q_u8(range_adjust_tbl, pos));
+
+            /* Load min and max values per calculated range index */
+            uint8x16_t minv = vqtbl1q_u8(range_min_tbl, range);
+            uint8x16_t maxv = vqtbl1q_u8(range_max_tbl, range);
+
+            /* Check value range */
+            error = vorrq_u8(error, vcltq_u8(input, minv));
+            error = vorrq_u8(error, vcgtq_u8(input, maxv));
+
+            prev_input = input;
+            prev_first_len = first_len;
 
             data += 16;
             len -= 16;
@@ -171,7 +198,7 @@ int utf8_range(const unsigned char *data, int len)
 
         /* Find previous token (not 80~BF) */
         uint32_t token4;
-        vst1q_lane_u32(&token4, vreinterpretq_u32_u8(previous_input.input), 3);
+        vst1q_lane_u32(&token4, vreinterpretq_u32_u8(prev_input), 3);
 
         const int8_t *token = (const int8_t *)&token4;
         int lookahead = 0;
@@ -181,6 +208,7 @@ int utf8_range(const unsigned char *data, int len)
             lookahead = 2;
         else if (token[1] > (int8_t)0xBF)
             lookahead = 3;
+
         data -= lookahead;
         len += lookahead;
     }
