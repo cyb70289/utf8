0e6034e1f3b4e14490e8b67de8b23f0a7ca8e707
Yibo Cai <cyb70289@gmail.com>
2018-12-11 06:26:14 +0000

    refine range2 implementation

--- range2-neon.c
+++ range2-neon.c
@@ -1,3 +1,7 @@
+/*
+ * Process 2x16 bytes in each iteration.
+ * Comments removed for brevity. See range-neon.c for details.
+ */
 #ifdef __aarch64__
 
 #include <stdio.h>
@@ -6,194 +10,123 @@
 
 int utf8_naive(const unsigned char *data, int len);
 
-struct previous_input {
-    uint8x16_t input;
-    uint8x16_t follow_bytes;
+static const uint8_t _first_len_tbl[] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3,
 };
 
-static const uint8_t _follow_tbl[] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 00 ~ BF */
-    1, 1,                               /* C0 ~ DF */
-    2,                                  /* E0 ~ EF */
-    3,                                  /* F0 ~ FF */
-};
-
-static const uint8_t _follow_mask_tbl[] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 00 ~ BF */
-    8, 8,                               /* C0 ~ DF */
-    8,                                  /* E0 ~ EF */
-    8,                                  /* F0 ~ FF */
+static const uint8_t _first_range_tbl[] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8,
 };
 
 static const uint8_t _range_min_tbl[] = {
-    /* 0,    1,    2,    3,    4,    5,    6,    7,    8 */
-    0x00, 0x80, 0x80, 0x80, 0xA0, 0x80, 0x90, 0x80, 0xC2,
-    /* Must be invalid */
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+    0x00, 0x80, 0x80, 0x80, 0xA0, 0x80, 0x90, 0x80,
+    0xC2, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
 };
-
 static const uint8_t _range_max_tbl[] = {
-    /* 0,    1,    2,    3,    4,    5,    6,    7,    8 */
-    0x7F, 0xBF, 0xBF, 0xBF, 0xBF, 0x9F, 0xBF, 0x8F, 0xF4,
-    /* Must be invalid */
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x7F, 0xBF, 0xBF, 0xBF, 0xBF, 0x9F, 0xBF, 0x8F,
+    0xF4, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 };
 
-/* uint8x16x2_t requires an interleaved table */
-static const uint8_t _minus_e0_tbl[] = {
-    /* index: 0~15, 16~31            */
-    /*  E0 -> */ 2,     3, /* <- F0  */
-                 0,     0,
-                 0,     0,
-                 0,     0,
-                 0,     4, /* <- F4  */
-                 0,     0,
-                 0,     0,
-                 0,     0,
-                 0,     0,
-                 0,     0,
-                 0,     0,
-                 0,     0,
-                 0,     0,
-    /*  ED -> */ 3,     0,
-                 0,     0,
-                 0,     0,
+static const uint8_t _range_adjust_tbl[] = {
+    2, 3, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0,
 };
-
-/* Get number of followup bytes to take care per high nibble */
-static inline uint8x16_t get_followup_bytes(
-        const uint8x16_t input, const uint8x16_t follow_table,
-        uint8x16_t *mask, const uint8x16_t mask_table)
-{
-    const uint8x16_t high_nibbles = vshrq_n_u8(input, 4);
-
-    *mask = vqtbl1q_u8(mask_table, high_nibbles);
-    return vqtbl1q_u8(follow_table, high_nibbles);
-}
-
-static inline uint8x16_t validate(const unsigned char *data, uint8x16_t error,
-        struct previous_input *prev, const uint8x16_t tables[],
-        const uint8x16x2_t minus_e0)
-{
-    uint8x16_t range1;
-
-    const uint8x16_t input1 = vld1q_u8(data);
-
-    /* range is 8 if input=0xC0~0xFF, overlap will lead to 9, 10, 11 */
-    const uint8x16_t follow_bytes1 =
-        get_followup_bytes(input1, tables[0], &range1, tables[1]);
-
-    /* 2nd byte */
-    /* range |= (follow_bytes, prev.follow_bytes) << 1 byte */
-    range1 = vorrq_u8(range1, vextq_u8(prev->follow_bytes, follow_bytes1, 15));
-
-    /* 3rd byte */
-    uint8x16_t subp, sub1_3, sub1_4;
-    /* saturate sub 1 */
-    subp = vqsubq_u8(prev->follow_bytes, vdupq_n_u8(1));
-    sub1_3 = vqsubq_u8(follow_bytes1, vdupq_n_u8(1));
-    /* range1 |= (sub1, subp) << 2 bytes */
-    range1 = vorrq_u8(range1, vextq_u8(subp, sub1_3, 14));
-
-    /* 4th byte */
-    /* saturate sub 2 */
-    subp = vqsubq_u8(prev->follow_bytes, vdupq_n_u8(2));
-    sub1_4 = vqsubq_u8(follow_bytes1, vdupq_n_u8(2));
-    /* range1 |= (sub1, subp) << 3 bytes */
-    range1 = vorrq_u8(range1, vextq_u8(subp, sub1_4, 13));
-
-    /*
-     * Check special cases (not 80..BF)
-     * +------------+---------------------+-------------------+
-     * | First Byte | Special Second Byte | range table index |
-     * +------------+---------------------+-------------------+
-     * | E0         | A0..BF              | 4                 |
-     * | ED         | 80..9F              | 5                 |
-     * | F0         | 90..BF              | 6                 |
-     * | F4         | 80..8F              | 7                 |
-     * +------------+---------------------+-------------------+
-     */
-    /* shift1 = (input1, prev.input) << 1 byte */
-    uint8x16_t shift1 = vextq_u8(prev->input, input1, 15);
-    /* E0: +2    ED: +3    F0: +3    F4: +4 */
-    uint8x16_t pos = vsubq_u8(shift1, vdupq_n_u8(0xE0));
-    range1 = vaddq_u8(range1, vqtbl2q_u8(minus_e0, pos));
-
-    /* Check value range */
-    uint8x16_t minv = vqtbl1q_u8(tables[2], range1);
-    uint8x16_t maxv = vqtbl1q_u8(tables[3], range1);
-
-    /* errors |= ((input < min) | (input > max)) */
-    error = vorrq_u8(error, vcltq_u8(input1, minv));
-    error = vorrq_u8(error, vcgtq_u8(input1, maxv));
-
-    /*===============================================================*/
-    uint8x16_t range2, sub2;
-
-    const uint8x16_t input2 = vld1q_u8(data+16);
-
-    const uint8x16_t follow_bytes2 =
-        get_followup_bytes(input2, tables[0], &range2, tables[1]);
-
-    range2 = vorrq_u8(range2, vextq_u8(follow_bytes1, follow_bytes2, 15));
-
-    sub2 = vqsubq_u8(follow_bytes2, vdupq_n_u8(1));
-    range2 = vorrq_u8(range2, vextq_u8(sub1_3, sub2, 14));
-
-    sub2 = vqsubq_u8(follow_bytes2, vdupq_n_u8(2));
-    range2 = vorrq_u8(range2, vextq_u8(sub1_4, sub2, 13));
-
-    shift1 = vextq_u8(input1, input2, 15);
-    pos = vsubq_u8(shift1, vdupq_n_u8(0xE0));
-    range2 = vaddq_u8(range2, vqtbl2q_u8(minus_e0, pos));
-
-    minv = vqtbl1q_u8(tables[2], range2);
-    maxv = vqtbl1q_u8(tables[3], range2);
-
-    error = vorrq_u8(error, vcltq_u8(input2, minv));
-    error = vorrq_u8(error, vcgtq_u8(input2, maxv));
-    /*===============================================================*/
-
-    prev->input = input2;
-    prev->follow_bytes = follow_bytes2;
-
-    return error;
-}
 
 int utf8_range2(const unsigned char *data, int len)
 {
     if (len >= 32) {
-        struct previous_input previous_input;
+        uint8x16_t prev_input = vdupq_n_u8(0);
+        uint8x16_t prev_first_len = vdupq_n_u8(0);
 
-        previous_input.input = vdupq_n_u8(0);
-        previous_input.follow_bytes = vdupq_n_u8(0);
+        const uint8x16_t first_len_tbl = vld1q_u8(_first_len_tbl);
+        const uint8x16_t first_range_tbl = vld1q_u8(_first_range_tbl);
+        const uint8x16_t range_min_tbl = vld1q_u8(_range_min_tbl);
+        const uint8x16_t range_max_tbl = vld1q_u8(_range_max_tbl);
+        const uint8x16x2_t range_adjust_tbl = vld2q_u8(_range_adjust_tbl);
 
-        /* Cached constant tables */
-        uint8x16_t tables[4];
-
-        tables[0] = vld1q_u8(_follow_tbl);
-        tables[1] = vld1q_u8(_follow_mask_tbl);
-        tables[2] = vld1q_u8(_range_min_tbl);
-        tables[3] = vld1q_u8(_range_max_tbl);
-
-        uint8x16x2_t minus_e0 = vld2q_u8(_minus_e0_tbl);
+        const uint8x16_t const_1 = vdupq_n_u8(1);
+        const uint8x16_t const_2 = vdupq_n_u8(2);
+        const uint8x16_t const_e0 = vdupq_n_u8(0xE0);
 
         uint8x16_t error = vdupq_n_u8(0);
 
         while (len >= 32) {
-            error = validate(data, error, &previous_input, tables, minus_e0);
+            /*************************** block 1 *****************************/
+            const uint8x16_t input = vld1q_u8(data);
+
+            uint8x16_t high_nibbles = vshrq_n_u8(input, 4);
+
+            const uint8x16_t first_len =
+                vqtbl1q_u8(first_len_tbl, high_nibbles);
+
+            uint8x16_t range = vqtbl1q_u8(first_range_tbl, high_nibbles);
+
+            range =
+                vorrq_u8(range, vextq_u8(prev_first_len, first_len, 15));
+
+            uint8x16_t tmp1, tmp2;
+            tmp1 = vqsubq_u8(first_len, const_1);
+            tmp2 = vqsubq_u8(prev_first_len, const_1);
+            range = vorrq_u8(range, vextq_u8(tmp2, tmp1, 14));
+
+            tmp1 = vqsubq_u8(first_len, const_2);
+            tmp2 = vqsubq_u8(prev_first_len, const_2);
+            range = vorrq_u8(range, vextq_u8(tmp2, tmp1, 13));
+
+            uint8x16_t shift1 = vextq_u8(prev_input, input, 15);
+            uint8x16_t pos = vsubq_u8(shift1, const_e0);
+            range = vaddq_u8(range, vqtbl2q_u8(range_adjust_tbl, pos));
+
+            uint8x16_t minv = vqtbl1q_u8(range_min_tbl, range);
+            uint8x16_t maxv = vqtbl1q_u8(range_max_tbl, range);
+
+            error = vorrq_u8(error, vcltq_u8(input, minv));
+            error = vorrq_u8(error, vcgtq_u8(input, maxv));
+
+            /*************************** block 2 *****************************/
+            const uint8x16_t _input = vld1q_u8(data+16);
+
+            high_nibbles = vshrq_n_u8(_input, 4);
+
+            const uint8x16_t _first_len =
+                vqtbl1q_u8(first_len_tbl, high_nibbles);
+
+            uint8x16_t _range = vqtbl1q_u8(first_range_tbl, high_nibbles);
+
+            _range =
+                vorrq_u8(_range, vextq_u8(first_len, _first_len, 15));
+
+            tmp1 = vqsubq_u8(_first_len, const_1);
+            tmp2 = vqsubq_u8(first_len, const_1);
+            _range = vorrq_u8(_range, vextq_u8(tmp2, tmp1, 14));
+
+            tmp1 = vqsubq_u8(_first_len, const_2);
+            tmp2 = vqsubq_u8(first_len, const_2);
+            _range = vorrq_u8(_range, vextq_u8(tmp2, tmp1, 13));
+
+            shift1 = vextq_u8(input, _input, 15);
+            pos = vsubq_u8(shift1, const_e0);
+            _range = vaddq_u8(_range, vqtbl2q_u8(range_adjust_tbl, pos));
+
+            minv = vqtbl1q_u8(range_min_tbl, _range);
+            maxv = vqtbl1q_u8(range_max_tbl, _range);
+
+            error = vorrq_u8(error, vcltq_u8(_input, minv));
+            error = vorrq_u8(error, vcgtq_u8(_input, maxv));
+
+            /************************ next iteration *************************/
+            prev_input = _input;
+            prev_first_len = _first_len;
 
             data += 32;
             len -= 32;
         }
 
-        /* Delay error check till loop ends */
         if (vmaxvq_u8(error))
             return 0;
 
-        /* Find previous token (not 80~BF) */
         uint32_t token4;
-        vst1q_lane_u32(&token4, vreinterpretq_u32_u8(previous_input.input), 3);
+        vst1q_lane_u32(&token4, vreinterpretq_u32_u8(prev_input), 3);
 
         const int8_t *token = (const int8_t *)&token4;
         int lookahead = 0;
@@ -203,11 +136,11 @@ int utf8_range2(const unsigned char *data, int len)
             lookahead = 2;
         else if (token[1] > (int8_t)0xBF)
             lookahead = 3;
+
         data -= lookahead;
         len += lookahead;
     }
 
-    /* Check remaining bytes with naive method */
     return utf8_naive(data, len);
 }
 
--- range2-sse.c
+++ range2-sse.c
@@ -1,3 +1,7 @@
+/*
+ * Process 2x16 bytes in each iteration.
+ * Comments removed for brevity. See range-sse.c for details.
+ */
 #ifdef __x86_64__
 
 #include <stdio.h>
@@ -6,210 +10,141 @@
 
 int utf8_naive(const unsigned char *data, int len);
 
-struct previous_input {
-    __m128i input;
-    __m128i follow_bytes;
+static const int8_t _first_len_tbl[] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3,
 };
 
-static const int8_t _follow_tbl[] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 00 ~ BF */
-    1, 1,                               /* C0 ~ DF */
-    2,                                  /* E0 ~ EF */
-    3,                                  /* F0 ~ FF */
-};
-
-static const int8_t _follow_mask_tbl[] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 00 ~ BF */
-    8, 8,                               /* C0 ~ DF */
-    8,                                  /* E0 ~ EF */
-    8,                                  /* F0 ~ FF */
+static const int8_t _first_range_tbl[] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8,
 };
 
 static const int8_t _range_min_tbl[] = {
-    /* 0,    1,    2,    3,    4,    5,    6,    7,    8 */
-    0x00, 0x80, 0x80, 0x80, 0xA0, 0x80, 0x90, 0x80, 0xC2,
-    /* Must be invalid */
-    0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0x7F,
+    0x00, 0x80, 0x80, 0x80, 0xA0, 0x80, 0x90, 0x80,
+    0xC2, 0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0x7F,
 };
-
 static const int8_t _range_max_tbl[] = {
-    /* 0,    1,    2,    3,    4,    5,    6,    7,    8 */
-    0x7F, 0xBF, 0xBF, 0xBF, 0xBF, 0x9F, 0xBF, 0x8F, 0xF4,
-    /* Must be invalid */
-    0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
+    0x7F, 0xBF, 0xBF, 0xBF, 0xBF, 0x9F, 0xBF, 0x8F,
+    0xF4, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
 };
 
-/* E0: 2, ED: 3 */
 static const int8_t _df_ee_tbl[] = {
     0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0,
 };
-
-/* F0: 3; F4: 4 */
 static const int8_t _ef_fe_tbl[] = {
     0, 3, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 };
 
-/* Get number of followup bytes to take care per high nibble */
-static inline __m128i get_followup_bytes(
-        const __m128i input, const __m128i follow_table,
-        __m128i *mask, const __m128i mask_table)
-{
-    /* Why no _mm_srli_epi8 ? */
-    const __m128i high_nibbles =
-        _mm_and_si128(_mm_srli_epi16(input, 4), _mm_set1_epi8(0x0F));
-
-    *mask = _mm_shuffle_epi8(mask_table, high_nibbles);
-    return _mm_shuffle_epi8(follow_table, high_nibbles);
-}
-
-static inline __m128i validate(const unsigned char *data, __m128i error,
-       struct previous_input *prev, const __m128i tables[])
-{
-    __m128i range1;
-
-    const __m128i input1 = _mm_lddqu_si128((const __m128i *)data);
-
-    /* range is 8 if input=0xC0~0xFF, overlap will lead to 9, 10, 11 */
-    __m128i follow_bytes1 =
-        get_followup_bytes(input1, tables[0], &range1, tables[1]);
-
-    /* 2nd byte */
-    /* range = (follow_bytes, prev.follow_bytes) << 1 byte */
-    range1 = _mm_or_si128(
-            range1, _mm_alignr_epi8(follow_bytes1, prev->follow_bytes, 15));
-
-    /* 3rd byte */
-    __m128i subp, sub1_3, sub1_4;
-    /* saturate sub 1 */
-    subp = _mm_subs_epu8(prev->follow_bytes, _mm_set1_epi8(1));
-    sub1_3 = _mm_subs_epu8(follow_bytes1, _mm_set1_epi8(1));
-    /* range1 |= (sub1, subp) << 2 bytes */
-    range1 = _mm_or_si128(range1, _mm_alignr_epi8(sub1_3, subp, 14));
-
-    /* 4th byte */
-    /* saturate sub 2 */
-    subp = _mm_subs_epu8(prev->follow_bytes, _mm_set1_epi8(2));
-    sub1_4 = _mm_subs_epu8(follow_bytes1, _mm_set1_epi8(2));
-    /* range1 |= (sub1, subp) << 3 bytes */
-    range1 = _mm_or_si128(range1, _mm_alignr_epi8(sub1_4, subp, 13));
-
-    /*
-     * Check special cases (not 80..BF)
-     * +------------+---------------------+-------------------+
-     * | First Byte | Special Second Byte | range table index |
-     * +------------+---------------------+-------------------+
-     * | E0         | A0..BF              | 4                 |
-     * | ED         | 80..9F              | 5                 |
-     * | F0         | 90..BF              | 6                 |
-     * | F4         | 80..8F              | 7                 |
-     * +------------+---------------------+-------------------+
-     */
-    __m128i shift1, pos, _range, tmp;
-
-    /* shift1 = (input1, prev.input) << 1 byte */
-    shift1 = _mm_alignr_epi8(input1, prev->input, 15);
-    /*
-     * shift1:  | EF  F0 ... FE | FF  00  ... ... ... ...  DE | DF  E0 ... EE |
-     * pos:     | 0   1      15 | 16  17                   239| 240 241    255|
-     * pos-240: | 0   0      0  | 0   0                    0  | 0   1      15 |
-     * pos+112: | 112 113    127|           >= 128            |     >= 128    |
-     */
-    pos = _mm_sub_epi8(shift1, _mm_set1_epi8(0xEF));
-    /* E0: +2; ED: +3 */
-    /* 0~15 -> 112~127(bit[3:0]=0~15), others -> above 127(bit[7]=1) */
-    tmp = _mm_subs_epu8(pos, _mm_set1_epi8(240));
-    _range = _mm_shuffle_epi8(tables[4], tmp);
-    /* F0: +3; F4: +4 */
-    /* 240~255 -> 0~15, others -> 0 */
-    tmp = _mm_adds_epu8(pos, _mm_set1_epi8(112));
-    _range = _mm_add_epi8(_range, _mm_shuffle_epi8(tables[5], tmp));
-
-    range1 = _mm_add_epi8(range1, _range);
-
-    /* Check value range */
-    __m128i minv = _mm_shuffle_epi8(tables[2], range1);
-    __m128i maxv = _mm_shuffle_epi8(tables[3], range1);
-
-    /* error |= ((input < min) | (input > max)) */
-    error = _mm_or_si128(error, _mm_cmplt_epi8(input1, minv));
-    error = _mm_or_si128(error, _mm_cmpgt_epi8(input1, maxv));
-
-    /*===============================================================*/
-    __m128i range2, sub2;
-
-    const __m128i input2 = _mm_lddqu_si128((const __m128i *)(data+16));
-
-    __m128i follow_bytes2 =
-        get_followup_bytes(input2, tables[0], &range2, tables[1]);
-
-    range2 = _mm_or_si128(
-            range2, _mm_alignr_epi8(follow_bytes2, follow_bytes1, 15));
-
-    sub2 = _mm_subs_epu8(follow_bytes2, _mm_set1_epi8(1));
-    range2 = _mm_or_si128(range2, _mm_alignr_epi8(sub2, sub1_3, 14));
-
-    sub2 = _mm_subs_epu8(follow_bytes2, _mm_set1_epi8(2));
-    range2 = _mm_or_si128(range2, _mm_alignr_epi8(sub2, sub1_4, 13));
-
-    shift1 = _mm_alignr_epi8(input2, input1, 15);
-    pos = _mm_sub_epi8(shift1, _mm_set1_epi8(0xEF));
-    tmp = _mm_subs_epu8(pos, _mm_set1_epi8(240));
-    _range = _mm_shuffle_epi8(tables[4], tmp);
-    tmp = _mm_adds_epu8(pos, _mm_set1_epi8(112));
-    _range = _mm_add_epi8(_range, _mm_shuffle_epi8(tables[5], tmp));
-
-    range2 = _mm_add_epi8(range2, _range);
-
-    minv = _mm_shuffle_epi8(tables[2], range2);
-    maxv = _mm_shuffle_epi8(tables[3], range2);
-
-    error = _mm_or_si128(error, _mm_cmplt_epi8(input2, minv));
-    error = _mm_or_si128(error, _mm_cmpgt_epi8(input2, maxv));
-    /*===============================================================*/
-
-    prev->input = input2;
-    prev->follow_bytes = follow_bytes2;
-
-    return error;
-}
-
 int utf8_range2(const unsigned char *data, int len)
 {
     if (len >= 32) {
-        struct previous_input previous_input;
+        __m128i prev_input = _mm_set1_epi8(0);
+        __m128i prev_first_len = _mm_set1_epi8(0);
 
-        previous_input.input = _mm_set1_epi8(0);
-        previous_input.follow_bytes = _mm_set1_epi8(0);
-
-        /* Cached constant tables */
-        __m128i tables[6];
-
-        tables[0] = _mm_lddqu_si128((const __m128i *)_follow_tbl);
-        tables[1] = _mm_lddqu_si128((const __m128i *)_follow_mask_tbl);
-        tables[2] = _mm_lddqu_si128((const __m128i *)_range_min_tbl);
-        tables[3] = _mm_lddqu_si128((const __m128i *)_range_max_tbl);
-        tables[4] = _mm_lddqu_si128((const __m128i *)_df_ee_tbl);
-        tables[5] = _mm_lddqu_si128((const __m128i *)_ef_fe_tbl);
+        const __m128i first_len_tbl =
+            _mm_lddqu_si128((const __m128i *)_first_len_tbl);
+        const __m128i first_range_tbl =
+            _mm_lddqu_si128((const __m128i *)_first_range_tbl);
+        const __m128i range_min_tbl =
+            _mm_lddqu_si128((const __m128i *)_range_min_tbl);
+        const __m128i range_max_tbl =
+            _mm_lddqu_si128((const __m128i *)_range_max_tbl);
+        const __m128i df_ee_tbl =
+            _mm_lddqu_si128((const __m128i *)_df_ee_tbl);
+        const __m128i ef_fe_tbl =
+            _mm_lddqu_si128((const __m128i *)_ef_fe_tbl);
 
         __m128i error = _mm_set1_epi8(0);
 
         while (len >= 32) {
-            error = validate(data, error, &previous_input, tables);
+            /***************************** block 1 ****************************/
+            const __m128i input = _mm_lddqu_si128((const __m128i *)data);
+
+            __m128i high_nibbles =
+                _mm_and_si128(_mm_srli_epi16(input, 4), _mm_set1_epi8(0x0F));
+
+            __m128i first_len = _mm_shuffle_epi8(first_len_tbl, high_nibbles);
+
+            __m128i range = _mm_shuffle_epi8(first_range_tbl, high_nibbles);
+
+            range = _mm_or_si128(
+                    range, _mm_alignr_epi8(first_len, prev_first_len, 15));
+
+            __m128i tmp1, tmp2;
+            tmp1 = _mm_subs_epu8(first_len, _mm_set1_epi8(1));
+            tmp2 = _mm_subs_epu8(prev_first_len, _mm_set1_epi8(1));
+            range = _mm_or_si128(range, _mm_alignr_epi8(tmp1, tmp2, 14));
+
+            tmp1 = _mm_subs_epu8(first_len, _mm_set1_epi8(2));
+            tmp2 = _mm_subs_epu8(prev_first_len, _mm_set1_epi8(2));
+            range = _mm_or_si128(range, _mm_alignr_epi8(tmp1, tmp2, 13));
+
+            __m128i shift1, pos, range2;
+            shift1 = _mm_alignr_epi8(input, prev_input, 15);
+            pos = _mm_sub_epi8(shift1, _mm_set1_epi8(0xEF));
+            tmp1 = _mm_subs_epu8(pos, _mm_set1_epi8(240));
+            range2 = _mm_shuffle_epi8(df_ee_tbl, tmp1);
+            tmp2 = _mm_adds_epu8(pos, _mm_set1_epi8(112));
+            range2 = _mm_add_epi8(range2, _mm_shuffle_epi8(ef_fe_tbl, tmp2));
+
+            range = _mm_add_epi8(range, range2);
+
+            __m128i minv = _mm_shuffle_epi8(range_min_tbl, range);
+            __m128i maxv = _mm_shuffle_epi8(range_max_tbl, range);
+
+            error = _mm_or_si128(error, _mm_cmplt_epi8(input, minv));
+            error = _mm_or_si128(error, _mm_cmpgt_epi8(input, maxv));
+
+            /***************************** block 2 ****************************/
+            const __m128i _input = _mm_lddqu_si128((const __m128i *)(data+16));
+
+            high_nibbles =
+                _mm_and_si128(_mm_srli_epi16(_input, 4), _mm_set1_epi8(0x0F));
+
+            __m128i _first_len = _mm_shuffle_epi8(first_len_tbl, high_nibbles);
+
+            __m128i _range = _mm_shuffle_epi8(first_range_tbl, high_nibbles);
+
+            _range = _mm_or_si128(
+                    _range, _mm_alignr_epi8(_first_len, first_len, 15));
+
+            tmp1 = _mm_subs_epu8(_first_len, _mm_set1_epi8(1));
+            tmp2 = _mm_subs_epu8(first_len, _mm_set1_epi8(1));
+            _range = _mm_or_si128(_range, _mm_alignr_epi8(tmp1, tmp2, 14));
+
+            tmp1 = _mm_subs_epu8(_first_len, _mm_set1_epi8(2));
+            tmp2 = _mm_subs_epu8(first_len, _mm_set1_epi8(2));
+            _range = _mm_or_si128(_range, _mm_alignr_epi8(tmp1, tmp2, 13));
+
+            __m128i _range2;
+            shift1 = _mm_alignr_epi8(_input, input, 15);
+            pos = _mm_sub_epi8(shift1, _mm_set1_epi8(0xEF));
+            tmp1 = _mm_subs_epu8(pos, _mm_set1_epi8(240));
+            _range2 = _mm_shuffle_epi8(df_ee_tbl, tmp1);
+            tmp2 = _mm_adds_epu8(pos, _mm_set1_epi8(112));
+            _range2 = _mm_add_epi8(_range2, _mm_shuffle_epi8(ef_fe_tbl, tmp2));
+
+            _range = _mm_add_epi8(_range, _range2);
+
+            minv = _mm_shuffle_epi8(range_min_tbl, _range);
+            maxv = _mm_shuffle_epi8(range_max_tbl, _range);
+
+            error = _mm_or_si128(error, _mm_cmplt_epi8(_input, minv));
+            error = _mm_or_si128(error, _mm_cmpgt_epi8(_input, maxv));
+
+            /************************ next iteration **************************/
+            prev_input = _input;
+            prev_first_len = _first_len;
 
             data += 32;
             len -= 32;
         }
 
-        /* Delay error check till loop ends */
-        /* Reduce error vector, error_reduced = 0xFFFF if error == 0 */
         int error_reduced =
             _mm_movemask_epi8(_mm_cmpeq_epi8(error, _mm_set1_epi8(0)));
         if (error_reduced != 0xFFFF)
             return 0;
 
-        /* Find previous token (not 80~BF) */
-        int32_t token4 = _mm_extract_epi32(previous_input.input, 3);
-
+        int32_t token4 = _mm_extract_epi32(prev_input, 3);
         const int8_t *token = (const int8_t *)&token4;
         int lookahead = 0;
         if (token[3] > (int8_t)0xBF)
@@ -218,11 +153,11 @@ int utf8_range2(const unsigned char *data, int len)
             lookahead = 2;
         else if (token[1] > (int8_t)0xBF)
             lookahead = 3;
+
         data -= lookahead;
         len += lookahead;
     }
 
-    /* Check remaining bytes with naive method */
     return utf8_naive(data, len);
 }
 
